2ea21fc5437543ffb9a17ea730904ba552c7fd26
Philipp Kolbe PC <philipp.kolbe1902@gmail.com>
2019-02-10 14:32:02 +0100

    added async season constructor and getting matches only once per season

--- Server/public/javascripts/Season.js
+++ Server/public/javascripts/Season.js
@@ -1,24 +1,40 @@
 const Matchday = require('./Matchday');
 
+module.exports.createSeason = async (year, matches, areStandingsNeeded) => {
+    //console.log("createSeason()");
+    let season = new Season(year, matches, areStandingsNeeded);
+    await season.createMatchdays(year, matches, areStandingsNeeded);
+    return season;
+}
+
 class Season {
-    constructor(year, matches) {
+    constructor(year, matches, areStandingsNeeded) {
         //console.log("Creating new season: " + year);
         this.year = year;
         this.matchdays = [];
+    }
 
+    async createMatchdays(year, matches, areStandingsNeeded) {
+        const seasonWithoutStandings = this.getSeasonWithoutStandings(year);
+        //console.log("Season Creating Matchdays: " + ((areStandingsNeeded != false) ? "standingsNeeded" : "standingsNOTNeeded"));
         for (let matchDayNr = 0; matchDayNr < 34; matchDayNr++) {
-            let matchdayMatches = getMatchDayMatches(matchDayNr);
+            let matchdayMatches = this.getMatchDayMatches(matches, matchDayNr);
 
             if (matchdayMatches.length > 0)
-                this.matchdays.push(new Matchday(matchDayNr + 1, matchdayMatches));
+                this.matchdays.push(await Matchday.createMatchday(year, matchDayNr + 1, matchdayMatches, areStandingsNeeded, seasonWithoutStandings));
         }
     }
 
-    getMatchDayMatches(matchDayNr) {
+    getSeasonWithoutStandings(year) {
+        const seasonHandler = new Handler.SeasonHandler(year, year, false);
+        return await FileUpdater(seasonHandler);
+    }
+
+    getMatchDayMatches(matches, matchDayNr) {
         let matchdayMatches = [];
 
         for (let matchNr = 0; matchNr < 9; matchNr++) {
-            let m = this.matches[matchDayNr*9 + matchNr];
+            let m = matches[matchDayNr*9 + matchNr];
             if (m.MatchIsFinished)
                 matchdayMatches.push(m);
         }
@@ -26,5 +42,3 @@ class Season {
         return matchdayMatches;
     }
 }
-
-module.exports = Season;
