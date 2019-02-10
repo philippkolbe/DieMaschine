4683f06b91f386965d0da2d112e63ecd4565be72
Philipp Kolbe PC <philipp.kolbe1902@gmail.com>
2019-02-10 14:28:55 +0100

    Added Standings to every Matchday

--- Server/public/javascripts/Matchday.js
+++ Server/public/javascripts/Matchday.js
@@ -1,13 +1,36 @@
-let Match = require('./Match');
+const Match = require('./Match');
+const Handler = require('./Handler');
+const FileUpdater = require('./FileUpdater');
+
+module.exports.createMatchday = async (seasonYear, matchdayNr, matchdayMatches, areStandingsNeeded, seasonMatchdays) => {
+    //console.log("CreateMatchday(): "  + ((areStandingsNeeded != false) ? "standingsNeeded" : "standingsNOTNeeded"));
+    let md = new Matchday(seasonYear, matchdayNr, matchdayMatches, areStandingsNeeded, seasonMatchdays);
+
+    if (areStandingsNeeded != false)
+        await md.createStandings(seasonYear, matchdayNr, seasonMatchdays);
+
+    return md;
+};
 
 class Matchday {
-    constructor(matchDayNr, matchdayMatches) {
-        //console.log("Creating new matchday: " + matchDayNr);
-        this.matchDayNr = matchDayNr;
-        this.matches = [];
-        
+    constructor(seasonYear, matchdayNr, matchdayMatches, areStandingsNeeded, seasonMatches) {
+        //console.log("Creating new matchday: " + matchdayNr);
+        this.matchDayNr = matchdayNr;
         this.matches = matchdayMatches.map(m => new Match(m));
     }
-}
 
-module.exports = Matchday;
+    async createStandings(seasonYear, matchdayNr, seasonMatchdays) {
+        //console.log("Creating standings");
+        let promiseArr = ['', 'Home', 'Away', 'Form']
+            .map((str) => {
+                const handler = new Handler[str + 'StandingsHandler'](seasonYear, matchdayNr, seasonMatchdays);
+                return FileUpdater(handler);
+            });
+        [
+            this.standings,
+            this.homeStandings,
+            this.awayStandings,
+            this.formStandings
+        ] = await Promise.all(promiseArr);
+    }
+}
