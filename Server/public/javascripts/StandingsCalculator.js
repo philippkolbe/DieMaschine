982388b28e39ecee2de39e98c300b8ec5fadd52c
Philipp Kolbe PC <philipp.kolbe1902@gmail.com>
2019-02-10 14:34:50 +0100

    added possible inputtign of seasonMatchdays + cleaned up code

--- Server/public/javascripts/StandingsCalculator.js
+++ Server/public/javascripts/StandingsCalculator.js
@@ -2,18 +2,48 @@ const FileUpdater = require('./FileUpdater');
 const Handler = require('./Handler');
 const Standings = require('./Standings');
 
-module.exports = async function(season, matchday, nrOfGames, config) {
+module.exports.calculateStandings = async function(seasonYear, matchdayNr, matchdays, nrOfGames, config) {
+    matchdays = matchdays || [];
     nrOfGames = nrOfGames || 34;
     config = config || {};
 
-    const handler = new Handler.MatchdayHandler(season, 1, matchday);
-    let matchdays = [];
-    for (handler.i = handler.start; handler.i <= handler.end; handler.i++) {
-        matchdays.push(await FileUpdater(handler));
+    //console.log("Calculating Standings " + seasonYear + " " + matchdayNr + ": " + getTypeOfStandings(nrOfGames, config));
+    if (matchdays.length > 0) {
+        matchdays = await getMatchdays(seasonYear);
+        matchdays = spliceUnneeded(matchdays, matchdayNr, nrOfGames);
     }
-    
-    matchdays = matchdays.splice(-nrOfGames, nrOfGames);
+
+    //console.log("Got Matchdays: " + matchdays.length);
     let standings = new Standings(matchdays, config);
 
-    return standings;
+    return standings.standings;
+}
+
+async function getMatchdays(seasonYear) {
+    const handler = new Handler.SeasonHandler(seasonYear, seasonYear, false);
+    let season = await FileUpdater(handler);
+    
+    //console.log("SeasonMatchdays: " + season.matchdays.length);
+    return season.matchdays;
+}
+
+function spliceUnneeded(matchdays, matchdayNr, nrOfGames) {
+    if (matchdayNr)
+        matchdays = matchdays.splice(0, matchdayNr);
+    //console.log("after matchdaynrsplice" + matchdays.length);
+    if (nrOfGames)
+        matchdays = matchdays.splice(-nrOfGames, nrOfGames);
+    //console.log("after formsplice: " + matchdays.length);
+    return matchdays;
+}
+
+function getTypeOfStandings(nrOfGames, config) {
+    if (config.home)
+        return "home";
+    else if (config.away)
+        return "away";
+    else if (nrOfGames < 34)
+        return "form";
+    else
+        return "normal";
 }
